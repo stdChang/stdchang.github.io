ab38c72c444c3a00a20d02c02560bbdee4182a39
Steven <101471432+stdChang@users.noreply.github.com>
2024-03-24 05:31:47 -0700

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/'
+  //set base even though base is defaulted, 
+  //for future ref, this is the homepage or domain i guess
 })
