3ef8f62252ed5880645b454815fbfb1ce7365d33
Steven <101471432+stdChang@users.noreply.github.com>
2024-11-17 09:05:43 -0800

    Long 4 hours to make Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,73 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from '@mui/material/Button'
 import Confetti from './Confetti';
-import { Box } from '@mui/material'
+import { AppBar, Button, Box, Icon, IconButton, Menu, MenuItem, MenuList, Toolbar, Typography } from '@mui/material'
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
+const pages = ['About Me', 'Projects', 'Skillset', 'Resume'];
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [anchorNav, setAnchorNav] = useState(null);
+  const openMenu = (onClick) => {
+    setAnchorNav(onClick.currentTarget);
+  };
+  const closeMenu=()=>{
+    setAnchorNav(null);
+  };
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
       <Confetti></Confetti>
-      </div>
+      </div> */} 
+
+      <AppBar position='static'>
+          <Toolbar>
+            <IconButton size='large' edge='start' color='inherit' aria-label='watashi no ie' sx={{display:{xs: 'none', md: 'flex'}}}>
+              <HomeRoundedIcon/>
+            </IconButton>
+            <Typography variant='6' component='div' sx={{flexGrow: 1, display:{xs: 'none', md: 'flex'}}}> Youkoso, watashi no website </Typography>
+            <Box sx={{display:{xs: 'none', md: 'flex'}}}>
+              <Button color='inherit'>About Me</Button>
+              <Button color='inherit'>Projects</Button>
+              <Button color='inherit'>Skillset</Button>
+              <Button color='inherit'>Resume</Button>
+              {/* {pages.map((page)=>(
+               <Button color='inhert' key={page}>{page}</Button> 
+              ))}  */}
+            </Box>
+            <Box sx={{display:{xs: 'flex', md: 'none'}}}>
+              <IconButton size='large' edge='start' color='inherit' onClick={openMenu}>
+                <MenuRoundedIcon/>
+              </IconButton>
+              <Menu open={Boolean(anchorNav)} onClose={closeMenu} sx={{display:{xs: 'flex', md: 'none'}}}>
+                <MenuList>
+                  <MenuItem>About Me</MenuItem>
+                  <MenuItem>Projects</MenuItem>
+                  <MenuItem>Skillset</MenuItem>
+                  <MenuItem>Resume</MenuItem>
+                  {/* {pages.map((page)=>(<MenuItem key={page}>{page}</MenuItem>))} */}
+                </MenuList>
+              </Menu>
+            </Box>
+
+            <IconButton size='large' edge='start' color='inherit' aria-label='watashi no ie' sx={{display:{xs: 'flex', md: 'none'}}}>
+              <HomeRoundedIcon/>
+            </IconButton>
+            <Typography variant='6' component='div' sx={{flexGrow: 1, display:{xs: 'flex', md: 'none'}}}> Youkoso, watashi no website </Typography>
+          </Toolbar>
+        </AppBar>
+
 
       <Box component="section" justifyItems = 'center' sx={{p: 2}}>
         <button onClick={() => {
@@ -42,10 +89,8 @@ function App() {
           <img src = "/img/coolportrait.jpg" width = "600" height="450"/>
           <h1>スティーブン・チャン</h1>  
         </Box>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
+      <p> Edit <code>src/App.jsx</code> and save to test HMR </p>
       <p className="read-the-docs">
         Handcrafted by Steven Chang for 5 hours
       </p>
